517ca32ed9ff227fd746e661b2176ba89d37854d
tarnaldo88 <tarnaldo88@gmail.com>
2024-05-29 17:14:54 -0700

    Loaded in Image Assets

--- ProperForm/App.js
+++ ProperForm/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {NavigationContainer, DrawerActions} from "@react-navigation/native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import MainTabScreen from "./Components/Screens/MainTabScreen";
+import {DrawerContent} from "./Components/Screens/DrawerContent";
+
+function AppDrawer() {
+	return (
+		<Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+			<Drawer.Screen name="DrawHome" component={MainTabScreen} />
+		</Drawer.Navigator>
+	);
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+			<AppDrawer />
+		</NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ ProperForm/Screens/LoginScreen.js
@@ -0,0 +1,162 @@
+import React, {useState, useEffect} from "react";
+import {
+    View,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    AsyncStorage,
+    Text
+} from "react-native";
+import axios from "axios";
+import {logstyle} from "./Styles";
+import {Base64} from "js-base64";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "../AsyncStorage/config";
+import firebase from "firebase/app";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function LoginScreen({navigation}) {
+    const [testname, setTestname] = useState();
+    const [username, setUsername] = useState();
+    const [password, setPw] = useState();
+    const [token, setToken] = useState();
+
+    const getData = async () => {
+        try {
+            const userprofile = await AsyncStorage.getItem("userProfile");
+            const userProfile = JSON.parse(userprofile);
+            // console.log(userProfile.username);
+            // console.log(userProfile.token);
+            if (userProfile !== null) {
+                setUsername(userProfile.username);
+                setToken(userProfile.token);
+            }
+        } catch (e) {
+            //error reading value
+        }
+    };
+
+    const submitPressed = async () => {
+        login(username, password);
+        let arr = await VerifyAccount();
+        console.log(arr);
+        if (arr[1]) {
+            // alert("logged in");
+            console.log(arr[0]);
+            onSubmit(username, arr[2], arr[0]);
+            getData(setUsername, setToken);
+            return navigation.navigate("mainHome");
+        } else {
+            alert("Username or password incorrect \nPlease try again");
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const handleUsername = text => {
+        setUsername(text);
+    };
+    const handlePassword = text => {
+        setPw(text);
+    };
+    const login = (username, pass) => {
+        // alert("username: " + username + " password: " + pass);
+    };
+
+    const onSubmit = async (userName, userToken, UserID) => {
+        try {
+            await AsyncStorage.setItem(
+                "userProfile",
+                JSON.stringify({userID: UserID, username: userName, token: userToken})
+            );
+            console.log(UserID);
+            await AsyncStorage.setItem("signedIn", "true");
+            let test = await AsyncStorage.getItem("signedIn");
+            // console.log('Variable Test: ' + test);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const VerifyAccount = async () => {
+        var arr = [];
+        await axios
+            .get(
+                "http://52.53.203.248/ProperApi/api/Login/" +
+                    username +
+                    "/" +
+                    encryptPassword(),
+                {}
+            )
+            .then(response => {
+                arr.push(response.data.userID);
+                arr.push(response.data.equal);
+                arr.push(response.data.token);
+            });
+        return arr;
+    };
+
+    const encryptPassword = () => {
+        var encode = Base64.encode(password);
+        //console.log(encode);
+        return encode;
+    };
+
+    
+
+    return (
+        <View style={logstyle.container}>
+            <Text style={{height: 30}}>{testname}</Text>
+
+            <TextInput
+                style={logstyle.input}
+                underlineColorAndroid="transparent"
+                placeholder=" Username"
+                placeholderTextColor="#9a73ef"
+                autoCapitalize="none"
+                onChangeText={handleUsername}
+            />
+            <TextInput
+                style={logstyle.input}
+                secureTextEntry={true}
+                underlineColorAndroid="transparent"
+                placeholder=" Password"
+                placeholderTextColor="#9a73ef"
+                autoCapitalize="none"
+                onChangeText={handlePassword}
+            />
+            <TouchableOpacity
+                onPress={() => {
+                    console.log(
+                        "http://52.53.203.248/ProperApi/api/Login/" +
+                            username +
+                            "/" +
+                            encryptPassword(),
+                    );
+                    submitPressed();
+                    //navigation.navigate("LoggedHomeScreen");
+                }}
+            >
+                <Image
+                    source={require("./../../img/submit.png")}
+                    style={logstyle.submitButton}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("register")}>
+                <Image
+                    source={require("./../../img/register.png")}
+                    style={logstyle.submitButton}
+                />
+            </TouchableOpacity>
+        </View>
+    );
+}
+export {LoginScreen};
