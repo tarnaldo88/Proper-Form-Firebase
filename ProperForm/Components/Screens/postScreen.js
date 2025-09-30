303741c4cdb69d829cc0dedf040af0c0a85a723d
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-30 16:39:04 -0700

    changing postscreen to read from firebase instead of old db

--- ProperForm/Components/Screens/postScreen.js
+++ ProperForm/Components/Screens/postScreen.js
@@ -1,38 +1,76 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  FlatList,
   TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
-import {views, text, button, image} from "./Styles";
-import axios from "axios";
+import { text } from "./Styles";
+import { collection, query, where, getDocs, orderBy, limit, getDoc, doc } from 'firebase/firestore';
+import { db, auth } from '../firebase';
 
 class PostScreen extends Component {
 	constructor(props) {
-		//const { navigation } = this.props;
 		super(props);
 		this.state = {
-		  data: [
-			{id:123, name: "None", searched: true,  position:"", steps:0, image:"https://bootdey.com/img/Content/avatar/avatar1.png"},
-			{id:25, name: "Arnaldo", searched: false,  position:"", steps:0, image:"https://bootdey.com/img/Content/avatar/avatar1.png"},
-			{id:31, name: "Aman", searched: false,  position:"", steps:0, image:"https://bootdey.com/img/Content/avatar/avatar4.png"},
-			{id:24, name: "Ariel", searched: false, position:"", steps:0, image:"https://bootdey.com/img/Content/avatar/avatar5.png"} ,
-			{id:27, name: "Josh", searched: false, position:"", steps:0, image:"https://bootdey.com/img/Content/avatar/avatar7.png"} ,
-		  ],
-		 stepData: [ 
-			  {date: "2021-04-05T18:15:15" , steps: 10500, userID: 1}
-		],
-			champ: "None", 
-			steps: 0, 
-			champimg: "https://bootdey.com/img/Content/avatar/avatar7.png",
+			loading: true,
 			userSteps: 0,
+			userName: 'User',
+			userImage: 'https://img.icons8.com/color/96/000000/user-male-circle--v1.png',
+			today: new Date().toLocaleDateString()
+		};
+	}
+
+	componentDidMount() {
+		this.fetchTopSteps();
+	}
+
+	fetchUserSteps = async () => {
+		try {
+			const user = auth.currentUser;
+			if (!user) {
+				throw new Error('No user logged in');
+			}
+
+			// Get user's display name and photo URL
+			const userDoc = await getDoc(doc(db, 'users', user.uid));
+			const userData = userDoc.data();
+
+			// Get today's steps for the current user
+			const today = new Date();
+			today.setHours(0, 0, 0, 0);
+			
+			const stepsRef = collection(db, 'steps');
+			const q = query(
+				stepsRef,
+				where('userId', '==', user.uid),
+				where('date', '>=', today),
+				orderBy('date', 'desc'),
+				limit(1)
+			);
+
+			const querySnapshot = await getDocs(q);
+			let latestSteps = 0;
+
+			if (!querySnapshot.empty) {
+				// Get the latest steps entry
+				const latestDoc = querySnapshot.docs[0];
+				latestSteps = latestDoc.data().steps || 0;
+			}
+
+			this.setState({
+				userSteps: latestSteps,
+				userName: user.displayName || userData?.displayName || 'User',
+				userImage: user.photoURL || userData?.photoURL || 'https://img.icons8.com/color/96/000000/user-male-circle--v1.png',
+				loading: false
+			});
+		} catch (error) {
+			console.error("Error fetching steps data: ", error);
+			this.setState({ loading: false });
 		}
-		// this.getSteps();
-		const selectedItems = [ { id: 0, name: "", steps: 0},]
-	}	
+	};
 	
 	makeDate = () => {			
 		var day = new Date().getDate();
@@ -71,214 +109,246 @@ class PostScreen extends Component {
                 break;
             case "Ariel":
               this.props.navigation.navigate('friendA')
-                break;
             case "Aman":
               this.props.navigation.navigate('friendB')
                 break;
         }
       }
-
-	// getSteps(){
-	// 	let arr=[];
-	// 	var newArr = [];
-	// 	let i = 0, x = 0;
-	// 	let day = this.makeDate();
-	// 	console.log(this.makeDate());
-	// 	axios.get("http://52.53.203.248/ProperApi/api/Steps", {})
-    //         .then(
-    //             response => {             
-    //                 let goalWeight = response.data;
-	// 				for(i = 0;i<response.data.length;i++){
-	// 					arr.push(goalWeight[i]);
-	// 					// console.log(i + " = " + goalWeight[i].userID)
-	// 				}
-	// 				// console.log(arr.length);
-	// 				let today = false;
-	// 				let M = new Date().getMonth() + 1;
-	// 				var arrM;
-	// 				for(i = 0; i < arr.length; i++) {						
-	// 					let D = parseInt(day);
-	// 					let arrD = parseInt("0" + arr[i].date.substring(8,10));
-	// 					arrM = parseInt("0" + arr[i].date.substring(5,7));
-	// 					//make sure date of item is from today
-	// 					if(arrD == D && arrM == M){
-	// 						for(x = 0; x<this.state.data.length;x++){
-	// 							if( this.state.data[x].id === arr[i].userID){
-	// 								if(this.state.data[x].steps < arr[i].steps){
-	// 									this.state.data[x].steps = arr[i].steps;	
-	// 								}					
-	// 								this.state.data[x].searched = true;
-	// 								if(this.state.steps < this.state.data[x].steps){
-	// 									console.log(this.state.data[x].name + ":: " + this.state.data[x].steps + "UID " +  arr[i].userID)
-	// 									this.setState(({ steps }) => ({
-	// 										steps: this.state.data[x].steps
-	// 									}));
-	// 									this.setState(({ champ }) => ({
-	// 										champ: this.state.data[x].name
-	// 									}));
-	// 									// this.state.steps = this.state.data[x].steps;
-	// 									//this.state.champ= this.state.data[x].name;
-	// 									this.state.champimg = this.state.data[x].image;
-	// 									this.setState(({ champ }) => ({
-	// 										champimg: this.state.data[x].image
-	// 									}));
-	// 								}
-	// 							}
-	// 						}		
-	// 					}										
-	// 				}		
-    //         });  
-	// }
-	nav(rout){
-		if(rout){
-			var testArr = this.state.data;
-
-			console.log("test array = " + testArr[1].name);
-			return (this.props.navigation.navigate('rankings', testArr))
-		} else{
-			return (this.props.navigation.navigate('postSteps'))
+		navToPostSteps = () => {
+			this.props.navigation.navigate('postSteps');
 		}
-	
-	}
 
-		render() {				
-			var i = 0
-			for( i = 0; i < 4; i++){
-				if(this.state.champ === this.state.data[i].name){
-					let cha = this.state.data[i].name;					
-				}
-			}
-			return(		
-				<View style={styles.container}>
-					<Text style={styles.name}>Steps Champion with: {this.state.steps}</Text>
-					<TouchableOpacity style={styles.card} >
-						<View style={styles.cardHeader}>
-						<Image style={styles.icon} source={{uri:"https://img.icons8.com/emoji/452/trophy-emoji.png"}}/>
-						</View>
-						<Text style={styles.name}>{this.state.champ}</Text>
-						<Image style={styles.userImage} source={{uri:this.state.champimg}}/>
-						
-						<View style={styles.cardFooter}>
-						<View style={{alignItems:"center", justifyContent:"center"}}>
-							
-							<TouchableOpacity style={styles.followButton} onPress={()=> {this.nav(1)}}>
-							<Text style={styles.followButtonText}>View Rankings</Text>  
-							</TouchableOpacity>
-						</View>
-						</View>
-					</TouchableOpacity>			
-					<TouchableOpacity style={styles.stepsButton} onPress={()=> {this.nav(0)}}>
-					<Text style={styles.followButtonText} >Enter Your Steps</Text>  
-					</TouchableOpacity>
-					</View>				
+		navToRankings = () => {
+			this.props.navigation.navigate('rankings');
+		}
+
+		render() {
+		const { loading, userName, userSteps, userImage, today } = this.state;
+
+		if (loading) {
+			return (
+				<View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
+					<ActivityIndicator size="large" color="#4CAF50" />
+				</View>
 			);
 		}
-	}
-	export {PostScreen};
 
-    const styles = StyleSheet.create({
-		rowContainer: {
-			flexDirection: 'row',
-			flexWrap: 'wrap',		
-			paddingTop: 5,
-		  },
-      container:{
-        flex:1,
-        marginTop:20,
-      },
-      list: {
-        paddingHorizontal: 5,
-        backgroundColor:"#E6E6E6",
-      },
-      listContainer:{
-       alignItems:'center'
-      },
-      /******** card **************/
-      card:{
-        shadowColor: '#00000021',
-        shadowOffset: {
-          width: 0,
-          height: 6,
-        },
-        shadowOpacity: 0.37,
-        shadowRadius: 7.49,
-        elevation: 12,
-    
-        marginVertical: 5,
-        backgroundColor:"white",
-        flexBasis: '46%',
-        marginHorizontal: 5,
-      },
-      cardFooter: {
-        paddingVertical: 17,
-        paddingHorizontal: 16,
-        borderTopLeftRadius: 1,
-        borderTopRightRadius: 1,
-        flexDirection: 'row',
-        alignItems:"center", 
-        justifyContent:"center"
-      },
-      cardContent: {
-        paddingVertical: 12.5,
-        paddingHorizontal: 16,
-      },
-      cardHeader:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingTop: 12.5,
-        paddingBottom: 8,
-        paddingHorizontal: 16,
-        borderBottomLeftRadius: 1,
-        borderBottomRightRadius: 1,
-      },
-      userImage:{
-        height: 120,
-        width: 120,
-        borderRadius:60,
-        alignSelf:'center',
-        borderColor:"#DCDCDC",
-        borderWidth:3,
-      },
-      name:{
-        fontSize:18,
-        // flex:1,
-        alignSelf:'center',
-        color:"#008080",
-        fontWeight:'bold'
-      },
-      position:{
-        fontSize:14,
-        flex:1,
-        alignSelf:'center',
-        color:"#696969"
-      },
-      followButton: {
-        marginTop:0,
-        height:45,
-        width:200,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius:30,
-        backgroundColor: "#00BFFF",
-      },
-	  stepsButton: {
-        marginTop:10,
-		marginLeft:15,
-        height:45,
-        width:200,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius:30,
-        backgroundColor: "#9602cc",
-      },
-      followButtonText:{
-        color: "#FFFFFF",
-        fontSize:20,
-      },
-      icon:{
-        height: 20,
-        width: 20, 
-      }
-    }); 
+		return (
+			<View style={styles.container}>
+				<Text style={styles.title}>Today's Steps</Text>
+				<Text style={styles.date}>{today}</Text>
+				
+				<View style={styles.statsCard}>
+					<Image 
+						source={{ uri: userImage }} 
+						style={styles.userImage}
+						defaultSource={{ uri: 'https://img.icons8.com/color/96/000000/user-male-circle--v1.png' }}
+					/>
+					<Text style={styles.userName}>{userName}</Text>
+					<Text style={styles.stepsCount}>{userSteps.toLocaleString()}</Text>
+					<Text style={styles.stepsLabel}>STEPS TODAY</Text>
+				</View>
+
+				<View style={styles.buttonsContainer}>
+					<TouchableOpacity 
+						style={[styles.button, styles.primaryButton]} 
+						onPress={this.navToPostSteps}
+					>
+						<Text style={styles.buttonText}>➕ Log Your Steps</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity 
+						style={[styles.button, styles.secondaryButton]}
+						onPress={this.navToRankings}
+					>
+						<Text style={[styles.buttonText, { color: '#333' }]}>👥 View Leaderboard</Text>
+					</TouchableOpacity>
+				</View>
+
+				<View style={styles.tipsContainer}>
+					<Text style={styles.tipsTitle}>💡 Daily Tip</Text>
+					<Text style={styles.tipText}>
+						Aim for at least 10,000 steps a day for better health. 
+						You're {userSteps >= 10000 ? 'doing great!' : 'almost there!'}
+					</Text>
+				</View>
+			</View>
+		);
+	}
+}
+export {PostScreen};
+
+const styles = StyleSheet.create({
+  // Main container styles
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+    color: '#333',
+  },
+  date: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  
+  // Stats card styles
+  statsCard: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    padding: 25,
+    alignItems: 'center',
+    marginBottom: 20,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 15,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  stepsCount: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#4CAF50',
+    marginVertical: 10,
+  },
+  stepsLabel: {
+    fontSize: 16,
+    color: '#666',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+  },
+  
+  // Buttons container
+  buttonsContainer: {
+    marginBottom: 20,
+  },
+  button: {
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 8,
+    alignItems: 'center',
+  },
+  primaryButton: {
+    backgroundColor: '#4CAF50',
+  },
+  secondaryButton: {
+    backgroundColor: '#f0f0f0',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  
+  // Tips container
+  tipsContainer: {
+    backgroundColor: '#f0f9f0',
+    borderRadius: 12,
+    padding: 20,
+    marginTop: 20,
+  },
+  tipsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#2E7D32',
+  },
+  tipText: {
+    fontSize: 14,
+    color: '#333',
+    lineHeight: 20,
+  },
+  
+  // Card styles
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 15,
+    marginVertical: 10,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0',
+    marginBottom: 10,
+  },
+  cardContent: {
+    paddingVertical: 10,
+  },
+  cardFooter: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#f0f0f0',
+    marginTop: 10,
+  },
+  
+  // Utility classes
+  textCenter: {
+    textAlign: 'center',
+  },
+  mb10: {
+    marginBottom: 10,
+  },
+  mt10: {
+    marginTop: 10,
+  },
+  ml10: {
+    marginLeft: 10,
+  },
+  mr10: {
+    marginRight: 10,
+  },
+  p10: {
+    padding: 10,
+  },
+  flexRow: {
+    flexDirection: 'row',
+  },
+  flex1: {
+    flex: 1,
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  justifyCenter: {
+    justifyContent: 'center',
+  },
+  selfCenter: {
+    alignSelf: 'center',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  halfWidth: {
+    width: '50%',
+  }
+});
