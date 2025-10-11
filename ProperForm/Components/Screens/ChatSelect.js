db40f197a4c630aa9f2202f778e1e2c959dd859c
tarnaldo88 <tarnaldo88@gmail.com>
2025-10-11 01:02:04 -0700

    altering chat select

--- ProperForm/Components/Screens/ChatSelect.js
+++ ProperForm/Components/Screens/ChatSelect.js
@@ -12,72 +12,100 @@ import {
   TouchableOpacity
 } from 'react-native';
 import {views, text, button, image} from "./Styles";
+import { auth, db } from "../firebase";
+import { collection, query, where, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
 
- class ChatSelect extends Component {
-    constructor(props) {
-        //const { navigation } = this.props;
-        super(props);
-        this.state = {
-          data: [
-            {id:1, name: "John",   position:"",               image:"https://bootdey.com/img/Content/avatar/avatar1.png"},
-            {id:1, name: "Josh",   position:"",               image:"https://bootdey.com/img/Content/avatar/avatar7.png"},
-            {id:2, name: "Random",  position:"", image:"https://bootdey.com/img/Content/avatar/avatar5.png"} ,
-            {id:3, name: "TestTrainer", position:"",     image:"https://bootdey.com/img/Content/avatar/avatar4.png"} ,
-          ]
-        };
-      }    
-      clickEventListener(item) {
-        // console.log(item.name)
-        switch (item.name){
-            case "John":
-              this.props.navigation.navigate('Messages')
-                break;
-            case "Josh":
-              this.props.navigation.navigate('Messages')
-                break;
-            case "Ariel":
-              this.props.navigation.navigate('friendA')
-                break;
-            case "Aman":
-              this.props.navigation.navigate('friendB')
-                break;
-        }
+class ChatSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {id:1, name: "John",   position:"",               image:"https://bootdey.com/img/Content/avatar/avatar1.png", uid: "john_uid"},
+        {id:1, name: "Josh",   position:"",               image:"https://bootdey.com/img/Content/avatar/avatar7.png", uid: "josh_uid"},
+        {id:2, name: "Random",  position:"", image:"https://bootdey.com/img/Content/avatar/avatar5.png", uid: "random_uid"} ,
+        {id:3, name: "TestTrainer", position:"",     image:"https://bootdey.com/img/Content/avatar/avatar4.png", uid: "testtrainer_uid"} ,
+      ]
+    };
+  }
+
+  async clickEventListener(item) {
+    try {
+      const currentUser = auth.currentUser;
+      if (!currentUser) {
+        Alert.alert('Not signed in', 'Please sign in to start a chat.');
+        return;
       }
-    
-      render() {
-        return (
-          <View style={styles.container}>
-            <FlatList style={styles.list}
-              contentContainerStyle={styles.listContainer}
-              data={this.state.data}
-              horizontal={false}
-              numColumns={2}
-              keyExtractor= {(item) => {
-                return item.id;
-              }}
-              renderItem={({item}) => {
-                return (
-                  <TouchableOpacity style={styles.card} onPress={() => {this.clickEventListener(item)}}>
-                    <View style={styles.cardHeader}>
-                      <Image style={styles.icon} source={{uri:"https://img.icons8.com/flat_round/64/000000/hearts.png"}}/>
-                    </View>
-                    <Image style={styles.userImage} source={{uri:item.image}}/>
-                    <View style={styles.cardFooter}>
-                      <View style={{alignItems:"center", justifyContent:"center"}}>
-                        <Text style={styles.name}>{item.name}</Text>
-                        <Text style={styles.position}>{item.position}</Text>
-                        <TouchableOpacity style={styles.followButton} onPress={()=> this.clickEventListener(item)}>
-                          <Text style={styles.followButtonText}>Chat</Text>  
-                        </TouchableOpacity>
-                      </View>
-                    </View>
-                  </TouchableOpacity>
-                )
-              }}/>
-          </View>
-        );
+
+      const otherUserId = item.uid;
+      if (!otherUserId) {
+        Alert.alert('Missing user', 'Selected user is missing an id (uid).');
+        return;
       }
+
+      const a = currentUser.uid < otherUserId ? currentUser.uid : otherUserId;
+      const b = currentUser.uid < otherUserId ? otherUserId : currentUser.uid;
+      const participantsHash = `${a}_${b}`;
+
+      const convsRef = collection(db, 'conversations');
+      const q = query(convsRef, where('participantsHash', '==', participantsHash));
+      const snap = await getDocs(q);
+
+      let chatId;
+      if (!snap.empty) {
+        chatId = snap.docs[0].id;
+      } else {
+        const docRef = await addDoc(convsRef, {
+          participants: [currentUser.uid, otherUserId],
+          participantsHash,
+          createdAt: serverTimestamp(),
+          updatedAt: serverTimestamp(),
+          lastMessage: null,
+        });
+        chatId = docRef.id;
+      }
+
+      this.props.navigation.navigate('Messages', {
+        chatId,
+        otherUser: { uid: otherUserId, name: item.name, image: item.image },
+      });
+    } catch (e) {
+      Alert.alert('Chat error', e?.message || 'Unable to open chat');
     }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList style={styles.list}
+          contentContainerStyle={styles.listContainer}
+          data={this.state.data}
+          horizontal={false}
+          numColumns={2}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={styles.card} onPress={() => {this.clickEventListener(item)}}>
+                <View style={styles.cardHeader}>
+                  <Image style={styles.icon} source={{uri:"https://img.icons8.com/flat_round/64/000000/hearts.png"}}/>
+                </View>
+                <Image style={styles.userImage} source={{uri:item.image}}/>
+                <View style={styles.cardFooter}>
+                  <View style={{alignItems:"center", justifyContent:"center"}}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.position}>{item.position}</Text>
+                    <TouchableOpacity style={styles.followButton} onPress={()=> this.clickEventListener(item)}>
+                      <Text style={styles.followButtonText}>Chat</Text>  
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )
+          }}/>
+      </View>
+    );
+  }
     
     export {ChatSelect}
 
